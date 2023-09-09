78a1af17f24e82e3d3605dbbd743d8a25013a81a
Variann <kjartandth@gmail.com>
2023-09-09 21:07:07 +0200

    Added tab bar menu
    I used a AsyncAction as I know how to use AsyncAction, I'm not too familiar with creating K2 nodes. Achieves the same result as ImGuiMenu

---
+++ Source/ImGuiBlueprint/Private/K2Node_TabBarMenu.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "K2Node_TabBarMenu.h"
+
+UK2Node_TabBarMenu* UK2Node_TabBarMenu::ImGui_TabBarMenu(TArray<FString> Tabs)
+{
+	UK2Node_TabBarMenu* NewAsyncObject = NewObject<UK2Node_TabBarMenu>();
+	NewAsyncObject->TabNames = Tabs;
+	return NewAsyncObject;
+}
+
+void UK2Node_TabBarMenu::Activate()
+{
+	Super::Activate();
+	
+	// Passing a bool* to BeginTabItem() is similar to passing one to Begin():
+	// the underlying bool will be set to false when the tab is closed.
+	if (ImGui::BeginTabBar("MyTabBar"))
+	{
+		for (int n = 0; n < TabNames.Num(); n++)
+		{
+			if (ImGui::BeginTabItem(TCHAR_TO_UTF8(*TabNames[n])))
+			{
+				TabAdded.Broadcast(n);
+				ImGui::EndTabItem();
+			}
+		}
+			
+		ImGui::EndTabBar();
+	}
+
+	Finish.Broadcast();
+	RemoveFromRoot();
+}
